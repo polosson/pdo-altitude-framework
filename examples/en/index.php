5f0d17dc64e86757af23093bc2acca7e23babb43
polosson <polo@polosson.com>
2016-01-10 02:10:45 +0100

    Examples for class "Listing" (EN) DONE !

--- examples/en/index.php
+++ examples/en/index.php
@@ -48,16 +48,18 @@
 			</li>
 			<li><a href="#listing">Listing</a>
 				<ol>
-					<li><a href="#L1">The most simple</a></li>
-					<li><a href="#L2">Filtering</a></li>
-					<li><a href="#L3">Joints</a></li>
+					<li><a href="#L1">Simple use case</a></li>
+					<li><a href="#L2">Sorting</a></li>
+					<li><a href="#L3">Filtering</a></li>
+					<li><a href="#L4">Joints</a></li>
 				</ol>
 			</li>
 			<li><a href="#infos">Infos</a>
 				<ol>
-					<li><a href="#F1">Modify an entry</a></li>
-					<li><a href="#F2">Create an entry</a></li>
-					<li><a href="#F3">Adding columns</a></li>
+					<li><a href="#F1">Get an entry</a></li>
+					<li><a href="#F2">Modify an entry</a></li>
+					<li><a href="#F3">Create an entry</a></li>
+					<li><a href="#F4">Remove an entry</a></li>
 				</ol>
 			</li>
 		</ol>
@@ -169,12 +171,341 @@
 		<a id="listing"></a>
 		<section>
 			<h2>Listing</h2>
+			<article>
+				<a id="L1"></a>
+				<h3>Simple use case</h3>
+				<p>Here is the most simple way to use the object "Listing":</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+<span class="var">$users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>);
+
+<span class="operator">print_r</span>(<span class="var">$users</span>);</pre>
+				<p>This will return <b>all the entries</b> of table "users", like so:</p>
+				<pre>Array (
+    [0] => Array (
+        [id] => 1
+        [name] => Paul
+        [pseudo] => Polo
+        [age] => 34
+        [last_action] => 2016-01-14T00:00:00+01:00
+        [alive] => 1
+    )
+    [1] => Array ( .... )
+    [2] => Array ( .... )
+    [3] => Array ( .... )
+    [4] => Array (
+        [id] => 5
+        [name] => Henri
+        [pseudo] => Riton
+        [age] => 30
+        [last_action] => 2015-09-22T00:00:00+02:00
+        [alive] => 1
+    )
+)</pre>
+				<p>
+					It's a simple list of all entries of the table, unsorted. Each line of this list contains all informations of each entries, on the form of
+					a associative array.
+				</p>
+				<p>
+					You can <b>use a column</b> as <b>index</b> of the array (associative array). However, make sure that this column is a <u><i>Unique index</i></u>
+					for the table, so as not to overwrite some lines. To use the column "name" as index for the array, simply use the method
+					<span class="function">simplyList</span>(), this way:
+				</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+<span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>);
+
+<span class="var">$users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">simplifyList</span>(<span class="argument">"users"</span>);
+
+<span class="operator">print_r</span>(<span class="var">$users</span>);</pre>
+				<p>Which will return:</p>
+				<pre>Array (
+    [Paul] => Array (
+        [id] => 1
+        [name] => Paul
+        [pseudo] => Polo
+        [age] => 34
+        [last_action] => 2016-01-14T00:00:00+01:00
+        [alive] => 1
+    )
+    [Marcel]  => Array ( .... )
+    [Jacques] => Array ( .... )
+    [Julie]   => Array ( .... )
+    [Henri]   => Array (
+        [id] => 5
+        [name] => Henri
+        [pseudo] => Riton
+        [age] => 30
+        [last_action] => 2015-09-22T00:00:00+02:00
+        [alive] => 1
+    )
+)</pre>
+				<p>
+					To get only <b>some specific columns</b>, you must set the 2nd parameter of the method <span class="function">getListe</span>(), namely
+					<span class="argument">$want</span>. For instance, to get only the name and pseudo of all users, we'll do:
+				</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+<span class="var">$users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>, <span class="argument">"name,pseudo"</span>);
+
+<span class="operator">print_r</span>(<span class="var">$users</span>);</pre>
+				<p>Which will give us:</p>
+				<pre>Array(
+    [0] => Array (
+        [name] => Paul
+        [pseudo] => Polo
+    )
+    [1] => Array (
+        [name] => Marcel
+        [pseudo] => Mamar
+    )
+    [2] => Array (
+        [name] => Jacques
+        [pseudo] => Jack
+    )
+    [3] => Array (
+        [name] => Julie
+        [pseudo] => Grenouille
+    )
+    [4] => Array (
+        [name] => Henri
+        [pseudo] => Riton
+    )
+)</pre>
+				<p>
+					To get all columns, you just have to ommit <span class="argument">$want</span>, or set it to <i>null</i>, or give it the string
+					<span class="argument">"*"</span> (it's the default behavior).
+				</p>
+			</article>
+			<article>
+				<a id="L2"></a>
+				<h3>Sorting data</h3>
+				<p>
+					Let's see now the use of sorting. To <b>sort data</b>, we just have to add the parameters <span class="argument">$sortBy</span> and
+					<span class="argument">$order</span>. Here we'll sort the list of users according to their age, from the oldest to the youngest:
+				</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+<span class="var">$users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>, <span class="argument">"*"</span>, <span class="argument">"age"</span>, <span class="argument">"desc"</span>);
+
+<span class="operator">print_r</span>(<span class="var">$users</span>);</pre>
+				<p>Which will gie us:</p>
+				<pre>Array (
+    [0] => Array (
+        [id] => 2
+        [name] => Marcel
+        [pseudo] => Mamar
+        [age] => 75
+        [last_action] => 2012-04-24T00:00:00+02:00
+        [alive] => 0
+    )
+    [1] => Array ( ... <span class="comment">/* Paul, age => 34 */</span> ... )
+    [2] => Array ( ... <span class="comment">/* Julie, age => 32 */</span> ... )
+    [3] => Array ( ... <span class="comment">/* Henri, age => 30 */</span> ... )
+    [4] => Array (
+        [id] => 3
+        [name] => Jacques
+        [pseudo] => Jack
+        [age] => 22
+        [last_action] => 2014-10-28T00:00:00+01:00
+        [alive] => 1
+    )
+)</pre>
+				<p>
+					Please note that by default, the sort order is <span class="argument">"asc"</span> (ascending). If you want ascending, just then ignore
+					<span class="argument">$order</span>, or give it <i>null</i>, or the string <span class="argument">"asc"</span>.
+				</p>
+			</article>
+			<article>
+				<a id="L3"></a>
+				<h3>Filtering data</h3>
+				<p>
+					To <b>filter data</b>, we'll use the three parameters <span class="argument">$filter_key</span>, <span class="argument">$filter_comp</span>,
+					and <span class="argument">$filter</span> of method <span class="function">getListe</span>().<br />
+					Thereby, to quickly get only the users whose age exceeds or equals 34 years old, we can do:
+				</p><pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+<span class="var">$users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>, <span class="argument">"*"</span>, <span class="argument">"last_action"</span>, <span class="argument">"desc"</span>, <span class="argument">"age"</span>, <span class="argument">">="</span>, <span class="argument">34</span>);
+
+<span class="operator">print_r</span>(<span class="var">$users</span>);</pre>
+				<p>It will results in:</p>
+				<pre>Array (
+    [0] => Array (
+        [id] => 1
+        [name] => Paul
+        [pseudo] => Polo
+        [age] => 34
+        [last_action] => 2016-01-14T00:00:00+01:00
+        [alive] => 1
+    )
+    [1] => Array (
+        [id] => 2
+        [name] => Marcel
+        [pseudo] => Mamar
+        [age] => 75
+        [last_action] => 2012-04-24T00:00:00+02:00
+        [alive] => 0
+    )
+)</pre>
+				<p>
+					So we have only the two users whose age exceeds or equals 34 years old, sorted by their last activity date ('last_action').
+					Please note that we have used the operand <span class="argument">">="</span>. This operand corresponds to thoses used in SQL. So it's also possible to use
+					<span class="argument">"<="</span>, <span class="argument">"<"</span>, <span class="argument">">"</span>, <span class="argument">"="</span>,
+					<span class="argument">"!="</span>, <span class="argument">"LIKE"</span>, <span class="argument">"BETWEEN"</span>, etc.
+				</p>
+				<p>
+					OK now, how to only get the users aged at least 30 years <b>but</b> who are still alive?<br />
+					Here comes the <b>multiple filtering</b>! To do so, we'll use the method <span class="function">addFiltre</span>().
+					This method can be used as many times as we want, before the call to <span class="function">getListe</span>(). Once a filter is
+					defined with this method, it's no longer necessary to use filtering inside <span class="function">getListe</span>().<br />
+					Let's return to our example:
+				</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+
+<span class="var">$l</span><span class="operator">-></span><span class="function">addFiltre</span>(<span class="argument">"age"</span>,   <span class="argument">">"</span>, <span class="argument">30</span>);
+<span class="var">$l</span><span class="operator">-></span><span class="function">addFiltre</span>(<span class="argument">"alive"</span>, <span class="argument">"="</span>,  <span class="argument">1</span>);
+
+<span class="var">$users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>, <span class="argument">"*"</span>, <span class="argument">"last_action"</span>, <span class="argument">"desc"</span>);
+<span class="operator">print_r</span>(<span class="var">$users</span>);</pre>
+				<p>Which will returns:</p>
+				<pre>Array (
+    [0] => Array (
+        [id] => 1
+        [name] => Paul
+        [pseudo] => Polo
+        [age] => 34
+        [last_action] => 2016-01-14T00:00:00+01:00
+        [alive] => 1
+    )
+    [1] => Array (
+        [id] => 4
+        [name] => Julie
+        [pseudo] => Grenouille
+        [age] => 32
+        [last_action] => 2015-10-28T00:00:00+01:00
+        [alive] => 1
+    )
+)</pre>
+				<p>
+					Next, if for one reason or another, you must remove or modify filters, you just have to use the method <span class="function">resetFiltre</span>().<br />
+					Example of use:
+				</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+
+<span class="var">$l</span><span class="operator">-></span><span class="function">addFiltre</span>(<span class="argument">"age"</span>,   <span class="argument">">"</span>, <span class="argument">30</span>);
+<span class="var">$l</span><span class="operator">-></span><span class="function">addFiltre</span>(<span class="argument">"alive"</span>, <span class="argument">"="</span>,  <span class="argument">1</span>);
+<span class="var">$older_users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>);
+
+<span class="var">$l</span><span class="operator">-></span><span class="function">resetFiltre</span>();
+
+<span class="var">$l</span><span class="operator">-></span><span class="function">addFiltre</span>(<span class="argument">"age"</span>,   <span class="argument">"<="</span>, <span class="argument">30</span>);
+<span class="var">$youger_users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>);</pre>
+				<p>
+					Thereby the variable <span class="var">$older_users</span> will contain an array with users aged at least 30 years old, but who are still alive, and the
+					variable <span class="var">$youger_users</span> will contain users whose age is below or equals to 30 years old.
+				</p>
+				<p>
+					It's of course possible to modify the way filters are added. By default, the operand used between each filter is <b>"AND"</b>. You can specify
+					any other operan with the 4th parameter, <span class="argument">$logique</span> ('OR', 'NAND', 'NOR'...).
+				</p>
+				<p>
+					Finally, if you need to use a <b>SQL function</b> in the filter, you have the possibility to use the method <span class="function">setFiltreSQL</span>().<br />
+					Here is an example:
+				</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+
+<span class="var">$l</span><span class="operator">-></span><span class="function">setFiltreSQL</span>(<span class="argument">"`age` >= 30 AND `last_action` <= DATE_ADD(NOW(), INTERVAL -6 MONTH)"</span>);
+<span class="var">$users</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"users"</span>);</pre>
+				<p>
+					Which will give us the list of users aged at least 30 years old, and whose the most recent activity is at least 6 month old.<br />
+					However, use this method carefully, especially if you include some variables in the parameter <span class="argument">$filtre</span>, because it
+					opens possibilities of SQL injection. Make sure beforehand that the variables are safe.
+				</p>
+			</article>
+			<article>
+				<a id="L4"></a>
+				<h3>Joints</h3>
+				<p>
+					To illustrate joints, let's take the table <b>comments</b>. This table has two <b>columns, prefixed</b> with string <b>"FK_"</b>. This prefix
+					has been given in constant <a href="#I2"><span class="argument">FOREIGNKEYS_PREFIX</span></a>. The two columns that interest us are
+					<b>"FK_user_ID"</b> et <b>"FK_item_ID"</b>. Those two columns are present in the array <a href="#I2"><span class="argument">$RELATIONS</span></a>,
+					allowing Altitude to establish the relationship between these columns and the associated tables. If this array doesn't exists, joints will be
+					ignored, and the columns will only contain the IDs.<br />
+					Reminder of the array, written at configuration time:
+				</p>
+				<pre><span class="var">$RELATIONS</span> = <span class="operator">Array</span>(
+    <span class="argument">"FK_user_ID"</span>	=> <span class="operator">Array</span>('table' => <span class="argument">"users"</span>,	'alias' => <span class="argument">"user"</span>),
+    <span class="argument">"FK_item_ID"</span>	=> <span class="operator">Array</span>('table' => <span class="argument">"items"</span>,	'alias' => <span class="argument">"item"</span>),
+    <span class="argument">"FK_comment_ID"</span>	=> <span class="operator">Array</span>('table' => <span class="argument">"comments"</span>,	'alias' => <span class="argument">"comment"</span>)
+);</pre>
+				<p>
+					Right. Now, let's say we want to get the list of comments, but we want at the same time to retreive all informations of the user who wrote it,
+					and the informations about the item which was commented on. To do so, nothing more simple:
+				</p>
+				<pre>
+<span class="var">$l</span> = <span class="operator">new</span> <span class="function">Listing</span>();
+
+<span class="var">$comments</span> = <span class="var">$l</span><span class="operator">-></span><span class="function">getListe</span>(<span class="argument">"comments"</span>);
+<span class="operator">print_r</span>(<span class="var">$comments</span>);</pre>
+				<p>And we've got:</p>
+				<pre>Array (
+    [0] => Array (
+            [id] => 1
+            [date] => 2015-11-21T00:00:00+01:00
+            [FK_user_ID] => 1
+            [FK_item_ID] => 4
+            [text] => What a nice comment, gniuk gniuk!
+            [user] => Array
+                (
+                    [id] => 1
+                    [name] => Paul
+                    [pseudo] => Polo
+                    [age] => 34
+                    [last_action] => 2016-01-14
+                    [alive] => 1
+                )
+
+            [item] => Array
+                (
+                    [id] => 4
+                    [ref] => itemNiuk
+                    [FK_user_ID] => 3
+                    [FK_comment_ID] => 1
+                    [date_creation] => 2015-01-14
+                    [content] => {"leski":"mow","gniuk":"gniuk"}
+                )
+        )
+    [1] => Array (...
+    [2] => Array (...
+    ... etc.</pre>
+				<p>
+					Basically, there is nothing to do, the joints are automatically resolved. Note that columns "FK_user_ID" et "FK_item_ID" are
+					present, but the array contains two additionnal lines: <b>"user"</b>, and <b>"item"</b>. These key names correspond to
+					the value <span class="argument">"alias"</span> of the correspondence table <span class="argument">$RELATIONS</span>.
+				</p>
+				<p>
+					If, for some reason, you <b>don't want</b> to get joints, you just have to set the parameter <span class="argument">$withFK</span>
+					(9th position) to <b>FALSE</b>.
+				</p>
+			</article>
 		</section>
 		<br />
 		<hr>
 		<a id="infos"></a>
 		<section>
 			<h2>Infos</h2>
+			<article>
+				<a id="F1"></a>
+				<h3>Get an entry</h3>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+			</article>
 		</section>
 	</div>
 </body>
